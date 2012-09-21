3df4973fd0bdb23090651001c4727077f629b280
marceloquinta <marceloricardoquinta@gmail.com>
2012-09-20 23:39:36 -0300

    Fazendo a leitura pela primeira vez.

--- DevBot/src/br/com/devbot/features/LightForm.java
+++ DevBot/src/br/com/devbot/features/LightForm.java
@@ -29,11 +29,15 @@ public class LightForm extends FeatureForm {
         lightButton.setTextPosition(Label.BOTTOM);
         lightButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
-                String rcvd = sendAndReceiveMessage("sl");
-                rcvd = rcvd.substring(rcvd.length()-5, rcvd.length()-1);
-                lightButton.setText(rcvd);
+                updateLight();
             }
         });
         this.addComponent(BorderLayout.CENTER, lightButton);
+    }
+    
+    public void updateLight(){
+        String rcvd = sendAndReceiveMessage("sl");
+                rcvd = rcvd.substring(rcvd.length()-5, rcvd.length()-1);
+                lightButton.setText(rcvd);
     }
 }
