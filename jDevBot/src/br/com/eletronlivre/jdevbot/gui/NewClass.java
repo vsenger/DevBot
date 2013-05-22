fe07d6ecbf54fba064675656e509ffe24a0037ee
Vinicius Senger <vinicius@globalcode.com.br>
2013-05-22 17:52:25 -0300

    ui swing

---
+++ jDevBot/src/br/com/eletronlivre/jdevbot/gui/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.eletronlivre.jdevbot.gui;
+
+/**
+ *
+ * @author vsenger
+ */
+public class NewClass {
+  public static void main(String[] args) {
+    for(int x=3;x>0;x--) {
+      System.out.println("x " + x);
+    }
+  }
+}
